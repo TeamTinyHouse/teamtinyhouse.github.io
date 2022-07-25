c22d7db78e7f5b44eee3b4c828502ed0eef3715e
FrenchFry-King25 <68553646+FrenchFry-King25@users.noreply.github.com>
2022-07-25 13:14:20 -0400

    cloes button added to all pages

---
+++ burger.js
@@ -0,0 +1,23 @@
+document.getElementById("close_icon").style.display = "none";
+var close = false;
+document.getElementById('burger_icon').addEventListener('mouseover', () => {
+    if (!close) {
+        document.getElementById('burgerLinks').style.right = "5px"
+        setTimeout(() => {
+            document.getElementById("close_icon").style.display = "flex";
+            document.getElementById("burger_icon").style.display = "none";
+            close = true
+        }, 510)
+    }
+
+})
+document.getElementById('close_icon').addEventListener('click', () => {
+    if (close) {
+        document.getElementById('burgerLinks').style.right = "-161px"
+        setTimeout(() => {
+            document.getElementById("close_icon").style.display = "none";
+            document.getElementById("burger_icon").style.display = "flex";
+            close = false;
+        }, 510)
+    }
+})
